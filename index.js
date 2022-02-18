87ed54faa02be6eaca366e0ed506287ea2ee1625
Alexandra Scharfenberger <ascharfe55@gmail.com>
2022-02-17 21:55:56 -0700

    added flyover times

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 // index.js
 const {
   fetchMyIP,
-  fetchCoordsByIP
+  fetchCoordsByIP,
+  fetchISSFlyOverTimes
 } = require('./iss');
 
 // fetchMyIP((error, ip) => {
@@ -12,13 +13,23 @@ const {
 
 //   console.log('It worked! Returned IP:' , ip);
 // });
+// const callback = (error, ip) => {
+//   if (error) {
+//     console.log("It didn't work!" , error);
+//     return;
+//   }
+
+//   console.log('It worked! Returned IP:' , ip);
+// };
+//fetchMyIP(callback);
+//fetchCoordsByIP('174.3.200.118',(callback));
+const coords =  { latitude: 53.5222, longitude: -113.4551 }
 const callback = (error, ip) => {
   if (error) {
     console.log("It didn't work!" , error);
     return;
   }
 
-  console.log('It worked! Returned IP:' , ip);
+  console.log('It worked! Returned flyover times:' , ip);
 };
-//fetchMyIP(callback);
-//fetchCoordsByIP('174.3.200.118',(callback));
+//fetchISSFlyOverTimes({ latitude: 53.5222, longitude: -113.4551 }, callback);
